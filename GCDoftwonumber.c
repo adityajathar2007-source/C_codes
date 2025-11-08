3474bdc6c1dc3f47662449ff820069bb0c510de4
adityajathar2007-source <adityajathar2007@gmail.com>
2025-11-08 10:34:53 +0530

    GCD of numbers

---
+++ GCDoftwonumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+int a, b, temp;
+
+printf("Enter two integers: ");
+scanf("%d %d", &a, &b);
+
+    while(b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD is %d\n", a);
+    return 0;
+}
