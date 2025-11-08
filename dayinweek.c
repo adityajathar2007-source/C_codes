92fefccdc80f56435718c8448411eba545ada46d
adityajathar2007-source <adityajathar2007@gmail.com>
2025-11-08 10:30:55 +0530

    days in week

---
+++ dayinweek.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int week;
+    
+    printf("Enter week number (1-7): ");
+    scanf("%d", &week);
+
+    if (week == 1)
+        printf("Monday\n");
+    else if (week == 2)
+        printf("Tuesday\n");
+    else if (week == 3)
+        printf("Wednesday\n");
+    else if (week == 4)
+        printf("Thursday\n");
+    else if (week == 5)
+        printf("Friday\n");
+    else if (week == 6)
+        printf("Saturday\n");
+    else if (week == 7)
+        printf("Sunday\n");
+    else
+        printf("Invalid Input! Please enter week number between 1-7.\n");
+
+    return 0;
+}
